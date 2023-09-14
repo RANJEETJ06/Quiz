85ee1b60a56ec625947e50552ee26670d82f9438
Ranjeet Kumar Jena <ranjeetjena06@gmai.com>
2023-09-15 01:03:25 +0530

    added Node for data store locally using priority queue with updated top 10 score only

--- src/main/java/com/quiz/Data/DataTransferOutput.java
+++ src/main/java/com/quiz/Data/DataTransferOutput.java
@@ -7,8 +7,8 @@ public class DataTransferOutput {
     public DataTransferOutput(){
         String url="jdbc:mysql://localhost:3306/quiz";
         String user="root";
-        String password="";
-        String query="select * from `quiz`.Quiz_score";
+        String password="qaz123@QAZ";
+        String query="SELECT * FROM `quiz`.`Quiz_score` WHERE score<=100 ORDER BY score DESC limit 10";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection= DriverManager.getConnection(url,user,password);
---
+++ src/main/java/com/quiz/Data/Node.java
@@ -0,0 +1,25 @@
+package com.quiz.Data;
+
+import java.util.Comparator;
+
+public class Node implements Comparable<Node> {
+    private String name;
+    private int score;
+    public Node(String name,int score){
+        this.name=name;
+        this.score=score;
+    }
+    public Node(){}
+    public String getName() {
+        return name;
+    }
+    public int getScore() {
+        return score;
+    }
+
+
+    @Override
+    public int compareTo(Node o) {
+        return o.score-this.score;
+    }
+}
